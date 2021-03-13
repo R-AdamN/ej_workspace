76deca350e8d2fe143a76e0e5396a2bf5e7d0d99
R-adachi <nisizakakkz@gmail.com>
2021-03-13 12:04:17 +0900

    nowday_test make

---
+++ nowday_test/src/module-info.java
@@ -0,0 +1,2 @@
+module nowday_test {
+}
---
+++ nowday_test/src/to/msn/wings/nowday_test/chap01/Hello.java
@@ -0,0 +1,13 @@
+package to.msn.wings.nowday_test.chap01;
+
+import java.time.LocalDateTime;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		LocalDateTime time = LocalDateTime.now();
+		System.out.println(time);
+
+	}
+
+}
